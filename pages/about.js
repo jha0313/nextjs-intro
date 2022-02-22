d1d823ef5b5d7a79b5e2c8724f2bf3f1e1370af1
Jae Sang Ha <JaeSang.Ha@morningstar.com>
2022-02-22 11:10:51 -0800

    Creating simple Next JS app

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Seo from "../components/Seo";
+
+export default function About() {
+    return (
+        <div>
+            <Seo title="About"></Seo>
+            <h1>About</h1>
+        </div>
+    )
+}
