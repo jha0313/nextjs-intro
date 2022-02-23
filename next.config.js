6923f2296a5ff153e8572163656f90fb2e9feb7b
Jae Sang Ha <JaeSang.Ha@morningstar.com>
2022-02-22 20:50:42 -0800

    additional commits

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-/** @type {import('next').NextConfig} */
-
 const API_KEY = process.env.API_KEY;
 
 module.exports = {
@@ -8,7 +6,11 @@ module.exports = {
     return [
       {
         source: "/api/movies",
-        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+      {
+        source: "/api/movies/:id",
+        destination: `https://api.themoviedb.org/3/movie/:id?api_key=${API_KEY}`,
       }
     ]
   }
